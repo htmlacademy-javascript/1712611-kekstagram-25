0e22efa2bb26a7aa8216d162110dda3886b529eb
Irina-Melnik <IrinaMelnikJune@gmail.com>
2022-02-25 00:56:47 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function getRandNumFromRange(from, to) {
+  if (to < from) {
+    return console.warn('Second argument should be bigger then first');
+  } else {
+    return Math.floor(from + Math.random() * (to - from + 1));
+  }
+}
+
+getRandNumFromRange(10, 100);
+
+function isStringInLimit(string, limit) {
+  if (string.length < limit) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isStringInLimit('Я учу js', 10);
