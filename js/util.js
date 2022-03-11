e38df5adf31ab37edb5e4939b3eac5fb00fcf3c4
Irina-Melnik <IrinaMelnikJune@gmail.com>
2022-03-11 20:46:45 +0300

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,26 @@
+function getRandomNumberFromRange(from, to) {
+  if (to <= from || from < 0) {
+    throw new Error('Wrong arguments');
+  } else {
+    return Math.floor(from + Math.random() * (to - from + 1));
+  }
+}
+
+function getRandomArrayElement(elements) {
+  return elements[getRandomNumberFromRange(0, elements.length - 1)];
+}
+
+function isStringInLimit(string, limit) {
+  return string.length <= limit;
+}
+
+isStringInLimit('Я учу js', 10);
+
+function buildPath(path, id, extension) {
+  return path + id + extension;
+}
+
+export {getRandomNumberFromRange};
+export {getRandomArrayElement};
+export {isStringInLimit};
+export {buildPath};
