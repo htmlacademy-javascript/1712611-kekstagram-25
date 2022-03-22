0d90cd6d57858dae3f111af4b3a5340d6d6d7607
Irina-Melnik <IrinaMelnikJune@gmail.com>
2022-03-22 11:55:07 +0300

    Заполнение модального окна данными (адрес изображения)

--- js/popup.js
+++ js/popup.js
@@ -2,12 +2,25 @@ const pictures = document.querySelectorAll('.picture');
 const popup = document.querySelector('.big-picture');
 const popupCloseButton = popup.querySelector('.big-picture__cancel');
 
-popupCloseButton.onclick = function () {
-  popup.classList.add('hidden');
+const closeByEscape = (evt) => {
+  if (evt.key === 'Escape') {
+    popup.classList.add('hidden');
+    document.removeEventListener('keydown', closeByEscape);
+  }
 };
 
 for (const picture of pictures) {
-  picture.onclick = function () {
+  picture.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const src = picture.querySelector('img').getAttribute('src');
+    popup.querySelector('.big-picture__img img').setAttribute('src', src);
     popup.classList.remove('hidden');
-  };
+    document.addEventListener('keydown', closeByEscape);
+  });
 }
+
+popupCloseButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  popup.classList.add('hidden');
+  document.removeEventListener('keydown', closeByEscape);
+});
